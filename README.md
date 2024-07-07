ef21f015c25a3bdcfa2eb24ea9ef40c047bf1124
ahmed hassan <ahmedhsn787@gmail.com>
2024-07-07 21:02:14 +0300

    nav-bar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # E-commerce
+
+\e934
+
+<a href="#" class="relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:transform-origin-left after:ease-in-out after:duration-300 hover:after:scale-x-100 hover:text-link-secondary">
+  Shop Now
+</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "15PX",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "980px",
+      xl: "1200px",
+      "2xl": "1420px",
+    },
+    fontFamily: {
+      primary: "Inter",
+      secondary: "Jost",
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#000000",
+          hover: "#D10202",
+        },
+        secondary: "#EFEFEF",
+        link: {
+          DEFAULT: "rgba(132,132,132,1)",
+          primary: "rgba(132,132,132,1)",
+          border: "rgba(235,235,235,1)",
+        },
+        accent: {
+          DEFAULT: "#848484",
+          secondary: "#848280",
+          hover: "#D6D6D6",
+        },
+      },
+      fontSize: {},
+    },
   },
   plugins: [],
 };
